b21074b3d1d81c8d363aa26292e7caebae5c280d
Mozzidaze <drewli02@outlook.com>
2021-09-27 17:13:10 +0800

    Updated chess engine(mostly move)

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -19,3 +19,23 @@ class GameState():
         ]
         self.whiteToMove = True
         self.moveLog = []
+
+class Move():
+    # map keys to values
+    # ke : value
+    ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4,
+                   "5": 3, "6": 2, "7": 1, "8": 0}
+    rowsToRank = {v: k for k, v in ranksToRows.items()}
+    filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3,
+                   "e": 4, "f": 5, "g": 6, "h": 7}
+    colsToFiles = {v: k for k, v in filesToCols.items()}
+
+    def __init__(self, starSq, endSq, board):
+        self.startRow = starSq[0]
+        self.startCol = starSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+
+    def getChessNotation(self):
