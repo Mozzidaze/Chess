69fe1e189db54c06efc6df143d26b005b0e490c6
Mozzidaze <drewli02@outlook.com>
2021-09-28 17:25:19 +0800

    updated logic for moves to adept validMoves functions

--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -28,7 +28,9 @@ def main():
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     gs = ChessEngine.GameState()
-    print(os. getcwd())
+    validMoves = gs.getValidMoves()
+    moveMade = False  # flag variable for when a move is made
+
     loadImages()  # only once before the while loop
     running = True
     sqSelected = ()  # no square is selected, keep track of the last click of the user, tuple(row, col)
@@ -38,6 +40,7 @@ def main():
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
+            # Mouse handler
             elif e.type == p.MOUSEBUTTONDOWN:
                 location = p.mouse.get_pos()    # (x,y) location of mouse
                 col = location[0]//SQ_SIZE
@@ -51,9 +54,19 @@ def main():
                 if len(playerClicks) == 2:  # after 2nd click
                     move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
                     print(move.getChessNotation())
-                    gs.makeMove(move)
+                    if move in validMoves:
+                        gs.makeMove(move)
+                        moveMade = True
                     sqSelected = ()  # reset user clicks
                     playerClicks = []
+            elif e.type == p.KEYDOWN:
+                if e.key == p.K_z:  # undo when key z is pressed
+                    gs.undoMove()
+                    moveMade = True
+
+            if moveMade:
+                validMoves = gs.getValidMoves()
+                moveMade = False
 
             drawGameState(screen, gs)
             clock.tick(MAX_FPS)
